b887971d80555a76f799cdd6ed9840e72ea3dfa5
takehiro <takehiro-r133@hotmail.co.jp>
2024-11-20 00:09:42 +0900

    add test.html

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,17 @@ use net_wasabi::http::HttpClient;
 use noli::prelude::*;
 
 fn main() -> u64 {
-    let client = HttpClient::new();
-    match client.get("host.test".to_string(), 8000, "/test.html".to_string()) {
-        Ok(res) => {
-            print!("response:\n{:#?}", res);
-        }
-        Err(e) => {
-            print!("error:\n{:#?}", e);
-        }
+  let client = HttpClient::new();
+  match client.get("host.test".to_string(), 8000, "/test.html".to_string()) {
+    Ok(res) => {
+      print!("response:\n{:#?}", res);
     }
-    0
+
+    Err(e) => {
+      print!("error:\n{:#?}", e);
+    }
+  }
+  0
 }
 
 entry_point!(main);
