ebb65e139d0c1c7ce8023d457e747a562209f23f
takehiro <takehiro-r133@hotmail.co.jp>
2024-11-22 01:15:55 +0900

    add Attribute and Scriput related States for state machine

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -93,6 +93,42 @@ impl HtmlTokenizer {
       }
     }
   }
+
+  fn append_attribute(&mut self, c: char, is_name: bool) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          tag: _,
+          self_closing: _,
+          ref mut attributes,
+        } => {
+          let len = attributes.len();
+          assert!(len > 0);
+
+          attributes[len - 1].add_char(c, is_name);
+        }
+
+        _ => panic!("`latest_token` should be either StartTag"),
+      }
+    }
+  }
+
+  fn set_self_closing_flag(&mut self) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          tag: _,
+          ref mut self_closing,
+          attributes: _,
+        } => *self_closing = true,
+        _ => panic!("`latest_token` should be either StartTag"),
+      }
+    }
+  }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -249,6 +285,182 @@ impl Iterator for HtmlTokenizer {
           self.reconsume = true;
           self.state = State::AttributeName;
           self.start_new_attribute();
+        }
+
+        State::AttributeName => {
+          if c == ' ' || c == '/' || c == '>' || self.is_eof() {
+            self.reconsume = true;
+            self.state = State::AfterAttributeName;
+            continue;
+          }
+
+          if c == '=' {
+            self.state = State::BeforeAttributeName;
+            continue;
+          }
+
+          if c.is_ascii_uppercase() {
+            self.append_attribute(c.to_ascii_lowercase(), /*is_name*/ true);
+            continue;
+          }
+
+          self.append_attribute(c, /*is_name*/ true);
+        }
+
+        State::AfterAttributeName => {
+          if c == ' ' {
+            // 空白文字は無視する
+            continue;
+          }
+
+          if c == '/' {
+            self.state = State::SelfClosingStartTag;
+            continue;
+          }
+
+          if c == '=' {
+            self.state = State::BeforeAttributeValue;
+            continue;
+          }
+
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.reconsume = true;
+          self.state = State::AttributeName;
+          self.start_new_attribute();
+        }
+
+        State::BeforeAttributeValue => {
+          if c == ' ' {
+            // 空白文字は無視する
+            continue;
+          }
+
+          if c == '"' {
+            self.state = State::AttributeValueDoubleQuoted;
+            continue;
+          }
+
+          if c == '\'' {
+            self.state = State::AttributeValueSingleQuoted;
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::AttributeValueUnquoted;
+        }
+
+        State::AttributeValueDoubleQuoted => {
+          if c == '"' {
+            self.state = State::AfterAttributeValueQuoted;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_attribute(c, /*is_name*/ false);
+        }
+        State::AttributeValueSingleQuoted => {
+          if c == '\'' {
+            self.state = State::AfterAttributeValueQuoted;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_attribute(c, /*is_name*/ false);
+        }
+
+        State::AttributeValueUnquoted => {
+          if c == ' ' {
+            self.state = State::BeforeAttributeName;
+            continue;
+          }
+
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_attribute(c, /*is_name*/ false);
+        }
+
+        State::AfterAttributeValueQuoted => {
+          if c == ' ' {
+            self.state = State::BeforeAttributeName;
+            continue;
+          }
+
+          if c == '/' {
+            self.state = State::SelfClosingStartTag;
+            continue;
+          }
+
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.reconsume = true;
+          self.state = State::BeforeAttributeValue;
+        }
+
+        State::SelfClosingStartTag => {
+          if c == '>' {
+            self.set_self_closing_flag();
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            // invalid parse error.
+            return Some(HtmlToken::Eof);
+          }
+        }
+
+        State::ScriptData => {
+          if c == '<' {
+            self.state = State::ScriptDataLessThanSign;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          return Some(HtmlToken::Char(c));
+        }
+
+        State::ScriptDataLessThanSign => {
+          if c == '/' {
+            // 一時的なバッファを空文字でリセットする
+            self.buf = String::new();
+            self.state = State::ScriptDataEndTagOpen;
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::ScriptData;
+          return Some(HtmlToken::Char('<'));
         }
       }
     }
