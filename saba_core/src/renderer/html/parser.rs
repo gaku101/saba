07f650478cf916ba78f92a1f08b49ed61ff3d6c8
takehiro <takehiro-r133@hotmail.co.jp>
2024-11-27 00:28:10 +0900

    add rest methods for HtmlParser

--- saba_core/src/renderer/html/parser.rs
+++ saba_core/src/renderer/html/parser.rs
@@ -7,6 +7,7 @@ use crate::renderer::html::attribute::Attribute;
 use crate::renderer::html::token::HtmlToken;
 use crate::renderer::html::token::HtmlTokenizer;
 use alloc::rc::Rc;
+use alloc::string::String;
 use alloc::vec::Vec;
 use core::cell::RefCell;
 use core::str::FromStr;
@@ -121,6 +122,351 @@ impl HtmlParser {
     }
     false
   }
+
+  fn create_char(&self, c: char) -> Node {
+    let mut s = String::new();
+    s.push(c);
+    Node::new(NodeKind::Text(s))
+  }
+
+  fn insert_char(&mut self, c: char) {
+    let current = match self.stack_of_open_elements.last() {
+      Some(n) => n.clone(),
+      None => return,
+    };
+
+    if let NodeKind::Text(ref mut s) = current.borrow_mut().kind {
+      s.push(c);
+      return;
+    }
+
+    if c == '\n' || c == ' ' {
+      return;
+    }
+
+    let node = Rc::new(RefCell::new(self.create_char(c)));
+
+    if current.borrow().first_child().is_some() {
+      current
+        .borrow()
+        .first_child()
+        .unwrap()
+        .borrow_mut()
+        .set_next_sibling(Some(node.clone()));
+      node.borrow_mut().set_previous_sibling(Rc::downgrade(
+        &current
+          .borrow()
+          .first_child()
+          .expect("failed to get a first child"),
+      ));
+    } else {
+      current.borrow_mut().set_first_child(Some(node.clone()));
+    }
+    current.borrow_mut().set_last_child(Rc::downgrade(&node));
+    node.borrow_mut().set_parent(Rc::downgrade(&current));
+    self.stack_of_open_elements.push(node);
+  }
+
+  pub fn construct_tree(&mut self) -> Rc<RefCell<Window>> {
+    let mut token = self.t.next();
+
+    while token.is_some() {
+      match self.mode {
+        InsertionMode::Initial => {
+          if let Some(HtmlToken::Char(_)) = token {
+            token = self.t.next();
+            continue;
+          }
+
+          self.mode = InsertionMode::BeforeHtml;
+          continue;
+        }
+        InsertionMode::BeforeHtml => {
+          match token {
+            Some(HtmlToken::Char(c)) => {
+              if c == ' ' || c == '\n' {
+                token = self.t.next();
+                continue;
+              }
+            }
+            Some(HtmlToken::StartTag {
+              ref tag,
+              self_closing: _,
+              ref attributes,
+            }) => {
+              if tag == "html" {
+                self.insert_element(tag, attributes.to_vec());
+                self.mode = InsertionMode::BeforeHead;
+                token = self.t.next();
+                continue;
+              }
+            }
+            Some(HtmlToken::Eof) | None => {
+              return self.window.clone();
+            }
+            _ => {}
+          }
+          self.insert_element("html", Vec::new());
+          self.mode = InsertionMode::BeforeHead;
+          continue;
+        }
+        InsertionMode::BeforeHead => {
+          match token {
+            Some(HtmlToken::Char(c)) => {
+              if c == ' ' || c == '\n' {
+                token = self.t.next();
+                continue;
+              }
+            }
+            Some(HtmlToken::StartTag {
+              ref tag,
+              self_closing: _,
+              ref attributes,
+            }) => {
+              if tag == "head" {
+                self.insert_element(tag, attributes.to_vec());
+                self.mode = InsertionMode::InHead;
+                token = self.t.next();
+                continue;
+              }
+            }
+            Some(HtmlToken::Eof) | None => {
+              return self.window.clone();
+            }
+            _ => {}
+          }
+          self.insert_element("head", Vec::new());
+          self.mode = InsertionMode::InHead;
+          continue;
+        }
+        InsertionMode::InHead => {
+          match token {
+            Some(HtmlToken::Char(c)) => {
+              if c == ' ' || c == '\n' {
+                self.insert_char(c);
+                token = self.t.next();
+                continue;
+              }
+            }
+            Some(HtmlToken::StartTag {
+              ref tag,
+              self_closing: _,
+              ref attributes,
+            }) => {
+              if tag == "style" || tag == "script" {
+                self.insert_element(tag, attributes.to_vec());
+                self.original_insertion_mode = self.mode;
+                self.mode = InsertionMode::Text;
+                token = self.t.next();
+                continue;
+              }
+              if tag == "body" {
+                self.pop_until(ElementKind::Head);
+                self.mode = InsertionMode::AfterHead;
+                continue;
+              }
+              if let Ok(_element_kind) = ElementKind::from_str(tag) {
+                self.pop_until(ElementKind::Head);
+                self.mode = InsertionMode::AfterHead;
+                continue;
+              }
+            }
+            Some(HtmlToken::EndTag { ref tag }) => {
+              if tag == "head" {
+                self.mode = InsertionMode::AfterHead;
+                token = self.t.next();
+                self.pop_until(ElementKind::Head);
+                continue;
+              }
+            }
+            Some(HtmlToken::Eof) | None => {
+              return self.window.clone();
+            }
+          }
+          token = self.t.next();
+          continue;
+        }
+        InsertionMode::AfterHead => {
+          match token {
+            Some(HtmlToken::Char(c)) => {
+              if c == ' ' || c == '\n' {
+                self.insert_char(c);
+                token = self.t.next();
+                continue;
+              }
+            }
+            Some(HtmlToken::StartTag {
+              ref tag,
+              self_closing: _,
+              ref attributes,
+            }) => {
+              if tag == "body" {
+                self.insert_element(tag, attributes.to_vec());
+                token = self.t.next();
+                self.mode = InsertionMode::InBody;
+                continue;
+              }
+            }
+            Some(HtmlToken::Eof) | None => {
+              return self.window.clone();
+            }
+            _ => {}
+          }
+          self.insert_element("body", Vec::new());
+          self.mode = InsertionMode::InBody;
+          continue;
+        }
+        InsertionMode::InBody => {
+          match token {
+            Some(HtmlToken::StartTag {
+              ref tag,
+              self_closing: _,
+              ref attributes,
+            }) => match tag.as_str() {
+              "p" => {
+                self.insert_element(tag, attributes.to_vec());
+                token = self.t.next();
+                continue;
+              }
+              "h1" | "h2" => {
+                self.insert_element(tag, attributes.to_vec());
+                token = self.t.next();
+                continue;
+              }
+              "a" => {
+                self.insert_element(tag, attributes.to_vec());
+                token = self.t.next();
+                continue;
+              }
+              _ => {
+                token = self.t.next();
+              }
+            },
+            Some(HtmlToken::EndTag { ref tag }) => {
+              match tag.as_str() {
+                "body" => {
+                  self.mode = InsertionMode::AfterBody;
+                  token = self.t.next();
+                  if !self.contain_in_stack(ElementKind::Body) {
+                    // パースの失敗。トークンを無視する
+                    continue;
+                  }
+                  self.pop_until(ElementKind::Body);
+                  continue;
+                }
+                "html" => {
+                  if self.pop_current_node(ElementKind::Body) {
+                    self.mode = InsertionMode::AfterBody;
+                    assert!(self.pop_current_node(ElementKind::Html));
+                  } else {
+                    token = self.t.next();
+                  }
+                  continue;
+                }
+                "p" => {
+                  let element_kind =
+                    ElementKind::from_str(tag).expect("failed to convert string to ElementKind");
+                  token = self.t.next();
+                  self.pop_until(element_kind);
+                  continue;
+                }
+                "h1" | "h2" => {
+                  let element_kind =
+                    ElementKind::from_str(tag).expect("failed to convert string to ElementKind");
+                  token = self.t.next();
+                  self.pop_until(element_kind);
+                  continue;
+                }
+                "a" => {
+                  let element_kind =
+                    ElementKind::from_str(tag).expect("failed to convert string to ElementKind");
+                  token = self.t.next();
+                  self.pop_until(element_kind);
+                  continue;
+                }
+                _ => {
+                  token = self.t.next();
+                }
+              }
+            }
+            Some(HtmlToken::Eof) | None => {
+              return self.window.clone();
+            }
+            Some(HtmlToken::Char(c)) => {
+              self.insert_char(c);
+              token = self.t.next();
+              continue;
+            }
+          }
+        }
+        InsertionMode::Text => {
+          match token {
+            Some(HtmlToken::Eof) | None => {
+              return self.window.clone();
+            }
+            Some(HtmlToken::EndTag { ref tag }) => {
+              if tag == "style" {
+                self.pop_until(ElementKind::Style);
+                self.mode = self.original_insertion_mode;
+                token = self.t.next();
+                continue;
+              }
+              if tag == "script" {
+                self.pop_until(ElementKind::Script);
+                self.mode = self.original_insertion_mode;
+                token = self.t.next();
+                continue;
+              }
+            }
+            Some(HtmlToken::Char(c)) => {
+              self.insert_char(c);
+              token = self.t.next();
+              continue;
+            }
+            _ => {}
+          }
+          self.mode = self.original_insertion_mode;
+        }
+        InsertionMode::AfterBody => {
+          match token {
+            Some(HtmlToken::Char(_c)) => {
+              token = self.t.next();
+              continue;
+            }
+            Some(HtmlToken::EndTag { ref tag }) => {
+              if tag == "html" {
+                self.mode = InsertionMode::AfterAfterBody;
+                token = self.t.next();
+                continue;
+              }
+            }
+            Some(HtmlToken::Eof) | None => {
+              return self.window.clone();
+            }
+            _ => {}
+          }
+
+          self.mode = InsertionMode::InBody;
+        }
+        InsertionMode::AfterAfterBody => {
+          match token {
+            Some(HtmlToken::Char(_c)) => {
+              token = self.t.next();
+              continue;
+            }
+            Some(HtmlToken::Eof) | None => {
+              return self.window.clone();
+            }
+            _ => {}
+          }
+
+          // パースの失敗
+          self.mode = InsertionMode::InBody;
+        }
+      }
+    }
+    self.window.clone()
+  }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -134,284 +480,4 @@ pub enum InsertionMode {
   Text,
   AfterBody,
   AfterAfterBody,
-}
-
-pub fn construct_tree(&mut self) -> Rc<RefCell<Window>> {
-  let mut token = self.t.next();
-
-  while token.is_some() {
-    match self.mode {
-      InsertionMode::Initial => {
-        if let Some(HtmlToken::Char(_)) = token {
-          token = self.t.next();
-          continue;
-        }
-
-        self.mode = InsertionMode::BeforeHtml;
-        continue;
-      }
-      InsertionMode::BeforeHtml => {
-        match token {
-          Some(HtmlToken::Char(c)) => {
-            if c == ' ' || c == '\n' {
-              token = self.t.next();
-              continue;
-            }
-          }
-          Some(HtmlToken::StartTag {
-            ref tag,
-            self_closing: _,
-            ref attributes,
-          }) => {
-            if tag == "html" {
-              self.insert_element(tag, attributes.to_vec());
-              self.mode = InsertionMode::BeforeHead;
-              token = self.next();
-              continue;
-            }
-          }
-          Some(HtmlToken::Eof) | None => {
-            return self.window.clone();
-          }
-          _ => {}
-        }
-        self.insert_element("html", Vec::new());
-        self.mode = InsertionMode::BeforeHead;
-        continue;
-      }
-      InsertionMode::BeforeHead => {
-        match token {
-          Some(HtmlToken::Char(c)) => {
-            if c == ' ' || c == '\n' {
-              token = self.t.next();
-              continue;
-            }
-          }
-          Some(HtmlToken::StartTag {
-            ref tag,
-            self_closing: _,
-            ref attributes,
-          }) => {
-            if tag == "head" {
-              self.insert_element(tag, attributes.to_vec());
-              self.mode = InsertionMode::InHead;
-              token = self.t.next();
-              continue;
-            }
-          }
-          Some(HtmlToken::Eof) | None => {
-            return self.window.clone();
-          }
-          _ => {}
-        }
-        self.insert_element("head", Vec::new());
-        self.mode = InsertionMode::InHead;
-        continue;
-      }
-      InsertionMode::InHead => {
-        match token {
-          Some(HtmlToken::Char(c)) => {
-            if c == ' ' || c == '\n' {
-              self.insert_char(c);
-              token = self.t.next();
-              continue;
-            }
-          }
-          Some(HtmlToken::StartTag {
-            ref tag,
-            self_closing: _,
-            ref attributes,
-          }) => {
-            if tag == "style" || tag == "script" {
-              self.insert_element(tag, attributes.to_vec());
-              self.original_insertion_mode = self.mode;
-              self.mode = InsertionMode::Text;
-              token = self.t.next();
-              continue;
-            }
-            if tag == "body" {
-              self.pop_until(ElementKind::Head);
-              self.mode = InsertionMode::AfterHead;
-              continue;
-            }
-            if let Ok(_element_kind) = ElementKind::from_str(tag) {
-              self.pop_until(ElementKind::Head);
-              self.mode = InsertionMode::AfterHead;
-              continue;
-            }
-          }
-          Some(HtmlToken::EndTag { ref tag }) => {
-            if tag == "head" {
-              self.mode = InsertionMode::AfterHead;
-              token = self.t.next();
-              self.pop_until(ElementKind::Head);
-              continue;
-            }
-          }
-          Some(HtmlToken::Eof) | None => {
-            return self.window.clone();
-          }
-        }
-        token = self.t.next();
-        continue;
-      }
-      InsertionMode::AfterHead => {
-        match token {
-          Some(HtmlToken::Char(c)) => {
-            if c == ' ' || c == '\n' {
-              self.insert_char(c);
-              token = self.t.next();
-              continue;
-            }
-          }
-          Some(HtmlToken::StartTag {
-            ref tag,
-            self_closing: _,
-            ref attributes,
-          }) => {
-            if tag == "body" {
-              self.insert_element(tag, attributes.to_vec());
-              token = self.t.next();
-              self.mode = InsertionMode::InBody;
-              continue;
-            }
-          }
-          Some(HtmlToken::Eof) | None => {
-            return self.window.clone();
-          }
-          _ => {}
-        }
-        self.insert_element("body", Vec::new());
-        self.mode = InsertionMode::InBody;
-        continue;
-      }
-      InsertionMode::InBody => {
-        match token {
-          Some(HtmlToken::StartTag {
-            ref tag,
-            self_closing: _,
-            ref attributes,
-          }) => match tag.as_str() {
-            "p" => {
-              self.insert_element(tag, attributes.to_vec());
-              token = self.t.next();
-              continue;
-            }
-            "h1" | "h2" => {
-              self.insert_element(tag, attributes.to_vec());
-              token = self.t.next();
-              continue;
-            }
-            "a" => {
-              self.insert_element(tag, attributes.to_vec());
-              token = self.t.next();
-              continue;
-            }
-            _ => {
-              token = self.t.next();
-            }
-          },
-          Some(HtmlToken::EndTag { ref tag }) => {
-            match tag.as_str() {
-              "body" => {
-                self.mode = InsertionMode::AfterBody;
-                token = self.t.next();
-                if !self.contain_in_stack(ElementKind::Body) {
-                  // パースの失敗。トークンを無視する
-                  continue;
-                }
-                self.pop_until(ElementKind::Body);
-                continue;
-              }
-              "html" => {
-                if self.pop_current_node(ElementKind::Body) {
-                  self.mode = InsertionMode::AfterBody;
-                  assert!(self.pop_current_node(ElementKind::Html));
-                } else {
-                  token = self.t.next();
-                }
-                continue;
-              }
-              _ => {
-                token = self.t.next();
-              }
-            }
-          }
-          Some(HtmlToken::Eof) | None => {
-            return self.window.clone();
-          }
-          Some(HtmlToken::Char(c)) => {
-            self.insert_char(c);
-            token = self.t.next();
-            continue;
-          }
-        }
-      }
-      InsertionMode::Text => {
-        match token {
-          Some(HtmlToken::Eof) | None => {
-            return self.window.clone();
-          }
-          Some(HtmlToken::EndTag { ref tag }) => {
-            if tag == "style" {
-              self.pop_until(ElementKind::Style);
-              self.mode = self.original_insertion_mode;
-              token = self.next();
-              continue;
-            }
-            if tag == "script" {
-              self.pop_until(ElementKind::Script);
-              self.mode = self.original_insertion_mode;
-              token = self.next();
-              continue;
-            }
-          }
-          Some(HtmlToken::Char(c)) => {
-            self.insert_char(c);
-            token = self.t.next();
-            continue;
-          }
-          _ => {}
-        }
-        self.mode = self.original_inesertion_mode;
-      }
-      InsertionMode::AfterBody => {
-        match token {
-          Some(HtmlToken::Char(_c)) => {
-            token = self.t.next();
-            continue;
-          }
-          Some(HtmlToken::EndTag { ref tag }) => {
-            if tag == "html" {
-              self.mode = InsertionMode::AfterAfterBody;
-              token = self.t.next();
-              continue;
-            }
-          }
-          Some(HtmlToken::Eof) | None => {
-            return self.window.clone();
-          }
-          _ => {}
-        }
-
-        self.mode = InsertionMode::InBody;
-      }
-      InsertionMode::AfterAfterBody => {
-        match token {
-          Some(HtmlToken::Char(_c)) => {
-            token = self.t.next();
-            continue;
-          }
-          Some(HtmlToken::Eof) | None => {
-            return self.window.clone();
-          }
-          _ => {}
-        }
-
-        // パースの失敗
-        self.mode = InsertionMode::InBody;
-      }
-    }
-  }
-  self.window.clone()
 }
