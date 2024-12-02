92cf239656568ad6642173b8cff05a17e1d57613
takehiro <takehiro-r133@hotmail.co.jp>
2024-12-03 01:08:30 +0900

    add some CssParser methods

--- saba_core/src/renderer/css/cssom.rs
+++ saba_core/src/renderer/css/cssom.rs
@@ -1,8 +1,11 @@
 use crate::alloc::string::ToString;
+use crate::renderer::css::token::CssToken;
 use crate::renderer::css::token::CssTokenizer;
 use alloc::string::String;
 use alloc::vec::Vec;
 use core::iter::Peekable;
+
+pub type ComponentValue = CssToken;
 
 #[derive(Debug, Clone)]
 pub struct CssParser {
@@ -12,6 +15,94 @@ pub struct CssParser {
 impl CssParser {
   pub fn new(t: CssTokenizer) -> Self {
     Self { t: t.peekable() }
+  }
+
+  pub fn parse_stylesheet(&mut self) -> StyleSheet {
+    let mut sheet = StyleSheet::new();
+    sheet.set_rules(self.consume_list_of_rules());
+  }
+
+  fn consume_list_of_rules(&mut self) -> Vec<QualifiedRule> {
+    let mut rules = Vec::new();
+
+    loop {
+      let token = match self.peek() {
+        Some(t) => t,
+        None => return rules,
+      };
+      match token {
+        CssToken::AtKeyword(_keyword) => {
+          let _rule = self.consume_qualified_rule();
+        }
+        _ => {
+          let rule = self.consume_qualified_rule();
+          match rule {
+            Some(r) => rules.push(r),
+            None => return rules,
+          }
+        }
+      }
+    }
+  }
+
+  fn consume_qualified_rule(&mut self) -> Option<QualifiedRule> {
+    let mut rule = QualifiedRule::new();
+
+    loop {
+      let token = match self.t.peek() {
+        Some(t) => t,
+        None => return None,
+      };
+
+      match token {
+        CssToken::OpenCurly => {
+          assert_eq!(self.next(), Some(CssToken::OpenCurly));
+          rule.set_declarations(self.consume_list_of_declarations());
+          return Some(rule);
+        }
+        _ => rule.set_selector(self.consume_selector()),
+      }
+    }
+  }
+
+  fn consume_selector(&mut self) -> Selector {
+    let token = match self.t.next() {
+      Some(t) => t,
+      None => panic!("should have a token but got None"),
+    };
+
+    match token {
+      CssToken::HashToken(value) => Selector::IdSelector(value[1..].to_string()),
+      CssToken::Delim(delim) => {
+        if delim == '.' {
+          return Selector::ClassSelector(self.consume_ident());
+        }
+        panic!("Parse error: {:?} is an unexpected token.", token);
+      }
+      CssToken::Ident(ident) => {
+        // a:hoverのようなセレクタはタグ名のセレクタとして扱うため、
+        // もしコロン（:）が出てきた場合は宣言ブロックの開始直前まで
+        // トークンを進める
+        if self.t.peek() == Some(&CssToken::Colon) {
+          while self.t.peek() != Some(&CssToken::OpenCurly) {
+            self.t.next();
+          }
+        }
+        Selector::TypeSelector(ident.to_string())
+      }
+      CssToken::AtKeyword(_keyword) => {
+        // @から始まるルールを無視するために、宣言ブロックの開始直前まで
+        // トークンを進める
+        while self.t.peek() != Some(&CssToken::OpenCurly) {
+          self.t.next();
+        }
+        Selector::UnknownSelector
+      }
+      _ => {
+        self.t.next();
+        Selector::UnknownSelector
+      }
+    }
   }
 }
 
@@ -73,5 +164,13 @@ impl Declaration {
       property: String::new(),
       value: ComponentValue::Ident(String::new()),
     }
+  }
+
+  pub fn set_property(&mut self, property: String) {
+    self.property = property
+  }
+
+  pub fn set_value(&mut self, value: ComponentValue) {
+    self.value = value
   }
 }
