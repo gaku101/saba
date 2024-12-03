be661c1db928336efa31af964dab6d842f4984a4
takehiro <takehiro-r133@hotmail.co.jp>
2024-12-04 00:55:41 +0900

    add rest of CssParser methods and tests

--- saba_core/src/renderer/css/cssom.rs
+++ saba_core/src/renderer/css/cssom.rs
@@ -19,14 +19,16 @@ impl CssParser {
 
   pub fn parse_stylesheet(&mut self) -> StyleSheet {
     let mut sheet = StyleSheet::new();
+
     sheet.set_rules(self.consume_list_of_rules());
+    sheet
   }
 
   fn consume_list_of_rules(&mut self) -> Vec<QualifiedRule> {
     let mut rules = Vec::new();
 
     loop {
-      let token = match self.peek() {
+      let token = match self.t.peek() {
         Some(t) => t,
         None => return rules,
       };
@@ -56,7 +58,7 @@ impl CssParser {
 
       match token {
         CssToken::OpenCurly => {
-          assert_eq!(self.next(), Some(CssToken::OpenCurly));
+          assert_eq!(self.t.next(), Some(CssToken::OpenCurly));
           rule.set_declarations(self.consume_list_of_declarations());
           return Some(rule);
         }
@@ -104,6 +106,78 @@ impl CssParser {
       }
     }
   }
+
+  fn consume_list_of_declarations(&mut self) -> Vec<Declaration> {
+    let mut declarations = Vec::new();
+
+    loop {
+      let token = match self.t.peek() {
+        Some(t) => t,
+        None => return declarations,
+      };
+
+      match token {
+        CssToken::CloseCurly => {
+          assert_eq!(self.t.next(), Some(CssToken::CloseCurly));
+
+          return declarations;
+        }
+        CssToken::SemiColon => {
+          assert_eq!(self.t.next(), Some(CssToken::SemiColon))
+        }
+        CssToken::Ident(ref _ident) => {
+          if let Some(declaration) = self.consume_declaration() {
+            declarations.push(declaration);
+          }
+        }
+        _ => {
+          self.t.next();
+        }
+      }
+    }
+  }
+
+  fn consume_declaration(&mut self) -> Option<Declaration> {
+    if self.t.peek().is_none() {
+      return None;
+    }
+
+    let mut declaration = Declaration::new();
+    declaration.set_property(self.consume_ident());
+
+    match self.t.next() {
+      Some(token) => match token {
+        CssToken::Colon => {}
+        _ => return None,
+      },
+      None => return None,
+    }
+
+    declaration.set_value(self.consume_component_value());
+
+    Some(declaration)
+  }
+
+  fn consume_ident(&mut self) -> String {
+    let token = match self.t.next() {
+      Some(t) => t,
+      None => panic!("should have a token but got None"),
+    };
+
+    match token {
+      CssToken::Ident(ref ident) => ident.to_string(),
+      _ => {
+        panic!("Parse error:{:?} is an unexpected token.", token)
+      }
+    }
+  }
+
+  fn consume_component_value(&mut self) -> ComponentValue {
+    self
+      .t
+      .next()
+      .expect("should have a token in consume_component_value")
+  }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -127,7 +201,7 @@ pub struct QualifiedRule {
   pub declarations: Vec<Declaration>,
 }
 
-impl QualifieedRule {
+impl QualifiedRule {
   pub fn new() -> Self {
     Self {
       selector: Selector::TypeSelector("".to_string()),
@@ -172,5 +246,122 @@ impl Declaration {
 
   pub fn set_value(&mut self, value: ComponentValue) {
     self.value = value
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use alloc::vec;
+
+  #[test]
+  fn test_empty() {
+    let style = "".to_string();
+    let t = CssTokenizer::new(style);
+    let cssom = CssParser::new(t).parse_stylesheet();
+
+    assert_eq!(cssom.rules.len(), 0);
+  }
+
+  #[test]
+  fn test_one_rule() {
+    let style = "p { color: red; }".to_string();
+    let t = CssTokenizer::new(style);
+    let cssom = CssParser::new(t).parse_stylesheet();
+
+    let mut rule = QualifiedRule::new();
+    rule.set_selector(Selector::TypeSelector("p".to_string()));
+    let mut declaration = Declaration::new();
+    declaration.set_property("color".to_string());
+    declaration.set_value(ComponentValue::Ident("red".to_string()));
+    rule.set_declarations(vec![declaration]);
+
+    let expected = [rule];
+    assert_eq!(cssom.rules.len(), expected.len());
+
+    let mut i = 0;
+    for rule in &cssom.rules {
+      assert_eq!(&expected[i], rule);
+      i += 1;
+    }
+  }
+
+  #[test]
+  fn test_id_selector() {
+    let style = "#id { color: red; }".to_string();
+    let t = CssTokenizer::new(style);
+    let cssom = CssParser::new(t).parse_stylesheet();
+
+    let mut rule = QualifiedRule::new();
+    rule.set_selector(Selector::IdSelector("id".to_string()));
+    let mut declaration = Declaration::new();
+    declaration.set_property("color".to_string());
+    declaration.set_value(ComponentValue::Ident("red".to_string()));
+    rule.set_declarations(vec![declaration]);
+
+    let expected = [rule];
+    assert_eq!(cssom.rules.len(), expected.len());
+
+    let mut i = 0;
+    for rule in &cssom.rules {
+      assert_eq!(&expected[i], rule);
+      i += 1;
+    }
+  }
+
+  #[test]
+  fn test_class_selector() {
+    let style = ".class { color: red; }".to_string();
+    let t = CssTokenizer::new(style);
+    let cssom = CssParser::new(t).parse_stylesheet();
+
+    let mut rule = QualifiedRule::new();
+    rule.set_selector(Selector::ClassSelector("class".to_string()));
+    let mut declaration = Declaration::new();
+    declaration.set_property("color".to_string());
+    declaration.set_value(ComponentValue::Ident("red".to_string()));
+    rule.set_declarations(vec![declaration]);
+
+    let expected = [rule];
+    assert_eq!(cssom.rules.len(), expected.len());
+
+    let mut i = 0;
+    for rule in &cssom.rules {
+      assert_eq!(&expected[i], rule);
+      i += 1;
+    }
+  }
+
+  #[test]
+  fn test_multiple_rules() {
+    let style = "p { content: \"Hey\"; } h1 { font-size: 40; color: blue; }".to_string();
+    let t = CssTokenizer::new(style);
+    let cssom = CssParser::new(t).parse_stylesheet();
+
+    let mut rule1 = QualifiedRule::new();
+    rule1.set_selector(Selector::TypeSelector("p".to_string()));
+    let mut declaration1 = Declaration::new();
+    declaration1.set_property("content".to_string());
+    declaration1.set_value(ComponentValue::StringToken("Hey".to_string()));
+    rule1.set_declarations(vec![declaration1]);
+
+    let mut rule2 = QualifiedRule::new();
+    rule2.set_selector(Selector::TypeSelector("h1".to_string()));
+    let mut declaration2 = Declaration::new();
+    declaration2.set_property("font-size".to_string());
+    declaration2.set_value(ComponentValue::Number(40.0));
+    let mut declaration3 = Declaration::new();
+    declaration3.set_property("color".to_string());
+    declaration3.set_value(ComponentValue::Ident("blue".to_string()));
+    rule2.set_declarations(vec![declaration2, declaration3]);
+
+    let expected = [rule1, rule2];
+    assert_eq!(cssom.rules.len(), expected.len());
+
+    let mut i = 0;
+    for rule in &cssom.rules {
+      assert_eq!(&expected[i], rule);
+      i += 1;
+    }
   }
 }
