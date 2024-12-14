da1c63e5ff1a9e711ebd955480d628b216efbc01
gaku101 <takehiro-r133@hotmail.co.jp>
2024-12-14 19:18:19 +0900

    add ast and runtime lib

---
+++ saba_core/src/renderer/js/ast.rs
@@ -0,0 +1,297 @@
+use crate::renderer::js::token::JsLexer;
+use crate::renderer::js::token::Token;
+use alloc::rc::Rc;
+use alloc::string::String;
+use alloc::vec::Vec;
+use core::iter::Peekable;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Node {
+  ExpressionStatement(Option<Rc<Node>>),
+  AdditiveExpression {
+    operator: char,
+    left: Option<Rc<Node>>,
+    right: Option<Rc<Node>>,
+  },
+  AssignmentExpression {
+    operator: char,
+    left: Option<Rc<Node>>,
+    right: Option<Rc<Node>>,
+  },
+  MemberExpression {
+    object: Option<Rc<Node>>,
+    property: Option<Rc<Node>>,
+  },
+  NumericLiteral(u64),
+  VariableDeclaration {
+    declarations: Vec<Option<Rc<Node>>>,
+  },
+  VariableDeclarator {
+    id: Option<Rc<Node>>,
+    init: Option<Rc<Node>>,
+  },
+  Identifier(String),
+  StringLiteral(String),
+}
+
+impl Node {
+  pub fn new_expression_statement(expression: Option<Rc<Self>>) -> Option<Rc<Self>> {
+    Some(Rc::new(Node::ExpressionStatement(expression)))
+  }
+
+  pub fn new_additive_expression(operator: char, left: Option<Rc<Node>>, right: Option<Rc<Node>>) -> Option<Rc<Self>> {
+    Some(Rc::new(Node::AdditiveExpression { operator, left, right }))
+  }
+
+  pub fn new_assignment_expression(operator: char, left: Option<Rc<Node>>, right: Option<Rc<Node>>) -> Option<Rc<Self>> {
+    Some(Rc::new(Node::AssignmentExpression { operator, left, right }))
+  }
+
+  pub fn new_member_expression(object: Option<Rc<Self>>, property: Option<Rc<Self>>) -> Option<Rc<Self>> {
+    Some(Rc::new(Node::MemberExpression { object, property }))
+  }
+
+  pub fn new_numeric_literal(value: u64) -> Option<Rc<Self>> {
+    Some(Rc::new(Node::NumericLiteral(value)))
+  }
+
+  pub fn new_variable_declarator(id: Option<Rc<Self>>, init: Option<Rc<Self>>) -> Option<Rc<Self>> {
+    Some(Rc::new(Node::VariableDeclarator { id, init }))
+  }
+
+  pub fn new_variable_declaration(declarations: Vec<Option<Rc<Self>>>) -> Option<Rc<Self>> {
+    Some(Rc::new(Node::VariableDeclaration { declarations }))
+  }
+
+  pub fn new_identifier(name: String) -> Option<Rc<Self>> {
+    Some(Rc::new(Node::Identifier(name)))
+  }
+
+  pub fn new_string_literal(value: String) -> Option<Rc<Self>> {
+    Some(Rc::new(Node::StringLiteral(value)))
+  }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Program {
+  body: Vec<Rc<Node>>,
+}
+
+impl Program {
+  pub fn new() -> Self {
+    Self { body: Vec::new() }
+  }
+
+  pub fn set_body(&mut self, body: Vec<Rc<Node>>) {
+    self.body = body;
+  }
+
+  pub fn body(&self) -> &Vec<Rc<Node>> {
+    &self.body
+  }
+}
+
+pub struct JsParser {
+  t: Peekable<JsLexer>,
+}
+
+impl JsParser {
+  pub fn new(t: JsLexer) -> Self {
+    Self { t: t.peekable() }
+  }
+
+  fn primary_expression(&mut self) -> Option<Rc<Node>> {
+    let t = match self.t.next() {
+      Some(token) => token,
+      None => return None,
+    };
+
+    match t {
+      Token::Identifier(value) => Node::new_identifier(value),
+      Token::StringLiteral(value) => Node::new_string_literal(value),
+      Token::Number(value) => Node::new_numeric_literal(value),
+      _ => None,
+    }
+  }
+
+  fn member_expression(&mut self) -> Option<Rc<Node>> {
+    self.primary_expression()
+  }
+
+  fn left_hand_side_expression(&mut self) -> Option<Rc<Node>> {
+    self.member_expression()
+  }
+
+  fn additive_expression(&mut self) -> Option<Rc<Node>> {
+    let left = self.left_hand_side_expression();
+
+    let t = match self.t.peek() {
+      Some(token) => token.clone(),
+      None => return left,
+    };
+
+    match t {
+      Token::Punctuator(c) => match c {
+        '+' | '-' => {
+          // '+'または'-'の記号を消費する
+          assert!(self.t.next().is_some());
+          Node::new_additive_expression(c, left, self.assignment_expression())
+        }
+        _ => left,
+      },
+      _ => left,
+    }
+  }
+
+  fn assignment_expression(&mut self) -> Option<Rc<Node>> {
+    let expr = self.additive_expression();
+
+    let t = match self.t.peek() {
+      Some(token) => token,
+      None => return expr,
+    };
+
+    match t {
+      Token::Punctuator('=') => {
+        // '='を消費する
+        assert!(self.t.next().is_some());
+        Node::new_assignment_expression('=', expr, self.assignment_expression())
+      }
+      _ => expr,
+    }
+  }
+
+  fn initialiser(&mut self) -> Option<Rc<Node>> {
+    let t = match self.t.next() {
+      Some(token) => token,
+      None => return None,
+    };
+
+    match t {
+      Token::Punctuator(c) => match c {
+        '=' => self.assignment_expression(),
+        _ => None,
+      },
+      _ => None,
+    }
+  }
+
+  fn identifier(&mut self) -> Option<Rc<Node>> {
+    let t = match self.t.next() {
+      Some(token) => token,
+      None => return None,
+    };
+
+    match t {
+      Token::Identifier(name) => Node::new_identifier(name),
+      _ => None,
+    }
+  }
+
+  fn variable_declaration(&mut self) -> Option<Rc<Node>> {
+    let ident = self.identifier();
+
+    let declarator = Node::new_variable_declarator(ident, self.initialiser());
+
+    let mut declarations = Vec::new();
+    declarations.push(declarator);
+
+    Node::new_variable_declaration(declarations)
+  }
+
+  fn statement(&mut self) -> Option<Rc<Node>> {
+    let t = match self.t.peek() {
+      Some(t) => t,
+      None => return None,
+    };
+
+    let node = match t {
+      Token::Keyword(keyword) => {
+        if keyword == "var" {
+          // "var"の予約語を消費する
+          assert!(self.t.next().is_some());
+
+          self.variable_declaration()
+        } else {
+          None
+        }
+      }
+      _ => Node::new_expression_statement(self.assignment_expression()),
+    };
+    if let Some(Token::Punctuator(c)) = self.t.peek() {
+      if c == &';' {
+        assert!(self.t.next().is_some())
+      }
+    }
+    node
+  }
+
+  fn source_element(&mut self) -> Option<Rc<Node>> {
+    match self.t.peek() {
+      Some(t) => t,
+      None => return None,
+    };
+    self.statement()
+  }
+
+  pub fn parse_ast(&mut self) -> Program {
+    let mut program = Program::new();
+
+    let mut body = Vec::new();
+
+    loop {
+      let node = self.source_element();
+
+      match node {
+        Some(n) => body.push(n),
+        None => {
+          program.set_body(body);
+          return program;
+        }
+      }
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use alloc::string::ToString;
+
+  #[test]
+  fn test_empty() {
+    let input = "".to_string();
+    let lexer = JsLexer::new(input);
+    let mut parser = JsParser::new(lexer);
+    let expected = Program::new();
+    assert_eq!(expected, parser.parse_ast());
+  }
+
+  #[test]
+  fn test_num() {
+    let input = "42".to_string();
+    let lexer = JsLexer::new(input);
+    let mut parser = JsParser::new(lexer);
+    let mut expected = Program::new();
+    let mut body = Vec::new();
+    body.push(Rc::new(Node::ExpressionStatement(Some(Rc::new(Node::NumericLiteral(42))))));
+    expected.set_body(body);
+    assert_eq!(expected, parser.parse_ast());
+  }
+
+  #[test]
+  fn test_add_nums() {
+    let input = "1 + 2".to_string();
+    let lexer = JsLexer::new(input);
+    let mut parser = JsParser::new(lexer);
+    let mut expected = Program::new();
+    let mut body = Vec::new();
+    body.push(Rc::new(Node::ExpressionStatement(Some(Rc::new(Node::AdditiveExpression {
+      operator: '+',
+      left: Some(Rc::new(Node::NumericLiteral(1))),
+      right: Some(Rc::new(Node::NumericLiteral(2))),
+    })))));
+    expected.set_body(body);
+    assert_eq!(expected, parser.parse_ast());
+  }
+}
--- saba_core/src/renderer/js/token.rs
+++ saba_core/src/renderer/js/token.rs
@@ -1,12 +1,17 @@
 use alloc::string::String;
-use alloc::vec::Vec;
 use alloc::string::ToString;
+use alloc::vec::Vec;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Token {
   Punctuator(char),
   Number(u64),
+  Identifier(String),
+  Keyword(String),
+  StringLiteral(String),
 }
+
+static RESERVED_WORDS: [&str; 1] = ["var"];
 
 pub struct JsLexer {
   pos: usize,
@@ -18,6 +23,62 @@ impl JsLexer {
     Self {
       pos: 0,
       input: js.chars().collect(),
+    }
+  }
+
+  fn contains(&self, keyword: &str) -> bool {
+    for i in 0..keyword.len() {
+      if keyword.chars().nth(i).expect("failed to access to i-th char") != self.input[self.pos + i] {
+        return false;
+      }
+    }
+
+    true
+  }
+
+  fn check_reserved_word(&self) -> Option<String> {
+    for word in RESERVED_WORDS {
+      if self.contains(word) {
+        return Some(word.to_string());
+      }
+    }
+
+    None
+  }
+
+  fn consume_identifier(&mut self) -> String {
+    let mut result = String::new();
+
+    loop {
+      if self.pos >= self.input.len() {
+        return result;
+      }
+
+      if self.input[self.pos].is_ascii_alphanumeric() || self.input[self.pos] == '$' {
+        result.push(self.input[self.pos]);
+        self.pos += 1;
+      } else {
+        return result;
+      }
+    }
+  }
+
+  fn consume_string(&mut self) -> String {
+    let mut result = String::new();
+    self.pos += 1;
+
+    loop {
+      if self.pos >= self.input.len() {
+        return result;
+      }
+
+      if self.input[self.pos] == '"' {
+        self.pos += 1;
+        return result;
+      }
+
+      result.push(self.input[self.pos]);
+      self.pos += 1;
     }
   }
 
@@ -52,12 +113,20 @@ impl Iterator for JsLexer {
       return None;
     }
 
+    // ホワイトスペースまたは改行文字が続く限り、次の位置に進める
     while self.input[self.pos] == ' ' || self.input[self.pos] == '\n' {
       self.pos += 1;
 
       if self.pos >= self.input.len() {
         return None;
       }
+    }
+
+    // 予約語が現れたら、Keywordトークンを返す
+    if let Some(keyword) = self.check_reserved_word() {
+      self.pos += keyword.len();
+      let token = Some(Token::Keyword(keyword));
+      return token;
     }
 
     let c = self.input[self.pos];
@@ -69,6 +138,8 @@ impl Iterator for JsLexer {
         t
       }
       '0'..='9' => Token::Number(self.consume_number()),
+      'a'..='z' | 'A'..='Z' | '_' | '$' => Token::Identifier(self.consume_identifier()),
+      '"' => Token::StringLiteral(self.consume_string()),
       _ => unimplemented!("char {:?} is not supported yet", c),
     };
     Some(token)
@@ -78,6 +149,7 @@ impl Iterator for JsLexer {
 #[cfg(test)]
 mod tests {
   use super::*;
+  use alloc::string::ToString;
 
   #[test]
   fn test_empty() {
@@ -91,6 +163,66 @@ mod tests {
     let input = "42".to_string();
     let mut lexer = JsLexer::new(input).peekable();
     let expected = [Token::Number(42)].to_vec();
+    let mut i = 0;
+    while lexer.peek().is_some() {
+      assert_eq!(Some(expected[i].clone()), lexer.next());
+      i += 1;
+    }
+    assert!(lexer.peek().is_none());
+  }
+
+  #[test]
+  fn test_add_nums() {
+    let input = "1 + 2".to_string();
+    let mut lexer = JsLexer::new(input).peekable();
+    let expected = [Token::Number(1), Token::Punctuator('+'), Token::Number(2)].to_vec();
+    let mut i = 0;
+    while lexer.peek().is_some() {
+      assert_eq!(Some(expected[i].clone()), lexer.next());
+      i += 1;
+    }
+    assert!(lexer.peek().is_none());
+  }
+
+  #[test]
+  fn test_assign_variable() {
+    let input = "var foo=\"bar\";".to_string();
+    let mut lexer = JsLexer::new(input).peekable();
+    let expected = [
+      Token::Keyword("var".to_string()),
+      Token::Identifier("foo".to_string()),
+      Token::Punctuator('='),
+      Token::StringLiteral("bar".to_string()),
+      Token::Punctuator(';'),
+    ]
+    .to_vec();
+    let mut i = 0;
+    while lexer.peek().is_some() {
+      assert_eq!(Some(expected[i].clone()), lexer.next());
+      i += 1;
+    }
+    assert!(lexer.peek().is_none());
+  }
+
+  #[test]
+  fn test_add_variable_and_num() {
+    let input = "var foo=42; var result=foo+1;".to_string();
+    let mut lexer = JsLexer::new(input).peekable();
+    let expected = [
+      Token::Keyword("var".to_string()),
+      Token::Identifier("foo".to_string()),
+      Token::Punctuator('='),
+      Token::Number(42),
+      Token::Punctuator(';'),
+      Token::Keyword("var".to_string()),
+      Token::Identifier("result".to_string()),
+      Token::Punctuator('='),
+      Token::Identifier("foo".to_string()),
+      Token::Punctuator('+'),
+      Token::Number(1),
+      Token::Punctuator(';'),
+    ]
+    .to_vec();
     let mut i = 0;
     while lexer.peek().is_some() {
       assert_eq!(Some(expected[i].clone()), lexer.next());
